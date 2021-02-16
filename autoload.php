6c1b13a5df223e599a72feae74cbcc8f6ad0bc47
TheKito <11139894+TheKito@users.noreply.github.com>
2021-02-16 05:10:42 -0300

    config

---
+++ autoload.php
@@ -0,0 +1,4 @@
+<?php
+
+//require_once __DIR__ . '/src/PathInterface.php';
+//require_once __DIR__ . '/src/Path.php';
---
+++ tests/TestTest.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class TestTest extends TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $this->assertSame(true, true);
+    }
+}
