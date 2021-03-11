52a2cd511322754f83c2bd1e6e7a2ade0e42b86e
TheKito <11139894+TheKito@users.noreply.github.com>
2021-03-11 12:57:13 -0300

    Create VirtualExceptionLoader.php

---
+++ src/VirtualExceptionLoader.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * PSR0Loader
+ * Loader old style
+ * php version 7.2.
+ *
+ * @category Loader
+ *
+ * @author   The Kito <thekito@blktech.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU GPL
+ *
+ * @link     https://github.com/TheKito/Loader
+ */
+
+namespace Kito\Loader;
+
+class VirtualExceptionLoader extends AbstractLoader
+{
+    /**
+     * Load virtual class file.
+     *
+     * @param string $className Class name with namespace
+     *
+     * @return bool Return true if class file its found and loaded
+     */
+    protected function loadClass(string $className): void
+    {
+        if (substr($className, -9) != 'Exception')
+            return;
+                
+//        if (count($_)>0) {
+//            eval('namespace ' . pathinfo($className,PATHINFO_DIRNAME) . '{ class ' . pathinfo($className,PATHINFO_FILENAME) . ' extends '.$extends.' {} }');
+//        } else {
+//            eval('class ' . pathinfo($className,PATHINFO_FILENAME) . ' extends '.$extends.' {}');
+//        }
+                
+    }
+}
