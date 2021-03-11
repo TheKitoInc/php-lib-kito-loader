48e7f7d942eb58b09b6c753b2a49f3ab4657c29f
TheKito <11139894+TheKito@users.noreply.github.com>
2021-03-11 13:01:35 -0300

    Create DummyLoader.php

---
+++ src/DummyLoader.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * DummyLoader
+ * DummyLoader
+ * php version 7.2.
+ *
+ * @category Loader
+ *
+ * @author   The Kito <thekito@blktech.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU GPL
+ *
+ * @link     https://github.com/TheKito/Loader
+ */
+
+namespace Kito\Loader;
+
+class DummyLoader extends AbstractLoader
+{
+
+    /**
+     * Print Dummy Load Class
+     *
+     * @param string $className Class name with namespace
+     *   
+     */
+    protected function loadClass(string $className): void
+    {
+        error_log("Dummy Loading: " . $className);
+    }
+}
