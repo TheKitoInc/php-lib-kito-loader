556e63df44667a3db7d3360e118a165132d6a978
TheKito <11139894+TheKito@users.noreply.github.com>
2021-03-12 13:47:06 -0300

    Create AbstractLoaderTest.php

---
+++ tests/AbstractLoaderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class AbstractLoaderTest extends TestCase
+{            
+    public function testFileExists(): string
+    {
+        $path = __DIR__ . '/src/AbstractLoader.php';
+        $this->assertFileExists($path);
+        return $path;
+    }
+}
