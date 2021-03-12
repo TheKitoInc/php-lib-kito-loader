230b21d6273ad04856d31108dcd6d8ece57bb963
TheKito <TheKito@users.noreply.github.com>
2021-03-12 17:14:32 +0000

    Apply fixes from StyleCI

--- tests/AbstractLoaderTest.php
+++ tests/AbstractLoaderTest.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
 class AbstractLoaderTest extends TestCase
-{            
+{
     public function testFileExists(): string
     {
-        $path = __DIR__ . '/src/AbstractLoader.php';
+        $path = __DIR__.'/src/AbstractLoader.php';
         $this->assertFileExists($path);
+
         return $path;
     }
 }
