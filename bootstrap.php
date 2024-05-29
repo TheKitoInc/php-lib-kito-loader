906bc1e64293a4dc356e564da054fd5852a3493d
Kito Git Service <git@thekitoinc.com>
2024-05-29 15:34:11 +0000

    Basic Bootstrap Update

--- bootstrap.php
+++ bootstrap.php
@@ -2,17 +2,17 @@
 
 spl_autoload_register(
     function ($classNameSpace) {
-        $classNameSpace = explode(DIRECTORY_SEPARATOR, str_replace('/', DIRECTORY_SEPARATOR, str_replace("\\", DIRECTORY_SEPARATOR, $classNameSpace)));
+        $classNameSpace = explode(DIRECTORY_SEPARATOR, str_replace('/', DIRECTORY_SEPARATOR, str_replace('\\', DIRECTORY_SEPARATOR, $classNameSpace)));
 
         foreach ($classNameSpace as $key => $value) {
-            if(empty($value) || $value == '.' || $value == '..') {
+            if (empty($value) || $value == '.' || $value == '..') {
                 unset($classNameSpace[$key]);
             }
         }
 
-        $classPath = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $classNameSpace) . '.php';
+        $classPath = __DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.implode(DIRECTORY_SEPARATOR, $classNameSpace).'.php';
 
-        if(file_exists($classPath)) {
+        if (file_exists($classPath)) {
             require_once $classPath;
         }
     }
